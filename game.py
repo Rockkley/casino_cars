974e1b57abf19548ead550ec6f2f55c707367e75
Evgeniy Samarin <rockkley94@gmail.com>
2022-10-12 08:41:22 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,121 +1,159 @@
-from ast import While
-import random
 import time
-from random import randint
+from random import randrange
+from operator import attrgetter
+# ~*~^~*~^~*~^~*~^~*~^~*~^~*~^~*~ #
+# leaders выводит только 1 из самых быстрых, даже если максимальуню из скоростей имеют несколько # fixme
+
 
 class Vehicle(object):
-    def __init__(self, id, name, speed, distance, win):
-        self.id = id
+    from_race: bool
+    boost: int
+    id_: int
+    name: str
+    speed: int
+    distance: int
+
+    def __init__(self, id_, name):
+        self.id_ = id_
         self.name = name
-        self.speed = speed
-        self.distance = distance
-        self.win = win
+        self.speed = 0
+        self.distance = 0
+        cars_list.append(self)
 
-    def start(self):
-        print(f'{self.name} стартует!\n')
-
-    def new_speed(self, boost):
+    def add_speed(self, boost):
         self.speed += boost
 
-    def stop(self):
-        print(f'Машина остановилась')
+    def start(self, duration: int = 10, from_race=False):
+        print(f'{self.name} стартует!')
+        if not from_race:
+            for second in range(1, duration+1):
+                self.add_speed(boost=randrange(5, 15))
+                self.distance = second * self.speed
+                print(f'{self.name} движется со скоростью {self.speed}, пройденное расстояние - {self.distance} метров')
+                time.sleep(3)
+            self.finish()
 
-    def winner(self):
-        print(f'{self.name} выиграл и проехал {self.distance} метров')
-        self.win = 1
+    def finish(self, from_race=False):
+        if not from_race:
+            print(f'{self.name} остановился, проехав {self.distance}')
+        self.speed = 0
+        self.distance = 0
 
-    def loser(self):
-        print(f'{self.name} проиграл и проехал {self.distance} метров')
-        self.win = 0
-    
-bank = 1000
 
-car_one = Vehicle('1', 'УАЗ', 1, 0, 0)
-car_two = Vehicle('2', 'Camry', 1, 0, 0)
+class Race:
+    duration: int
+    orig_duration: int
+    cars: list
+    bank: int
+    selected_car: int
+    bet: int
 
-def race():
+    def __init__(self, cars, bank, duration):
+        self.duration = duration
+        self.orig_duration = duration
+        self.cars = cars
+        self.bank = bank
+        self.selected_car = self.get_car_choice()
+        self.bet = self.get_bet()
+        self.start_race()
 
-    global bank
+    def get_car_choice(self) -> int:
+        """
+        :return: номер выбранной машины
+        """
+        selected_car = 0
 
-    print(f'Выберите машину:\n1) "УАЗ"\n2) "Camry"')
-    car_selected = input()
-
-    while True:
-        if car_selected not in ('1','2'):
-            print(f'Выберите машину:\n1) "УАЗ"\n2) "Camry"')
-            car_selected = input()
+        while selected_car not in range(1, len(self.cars) + 1):
+            print('Выберите машину:')
+            for number, car in enumerate(cars_list):
+                print(f"{number + 1}) {car.name}")
+            selected_car = int(input('>> '))
         else:
-            break
-        
-    print(f'Ваш банк: {bank}\nВведите вашу ставку:')
-    bet = input()
+            return selected_car
 
-    while True:
-        if int(bet) > bank:
-            print(f'У вас нет столько денег!\nВведите другую сумму!{bank}')
-            bet = input()
+    def get_bet(self) -> int:
+        """
+        :return: размер ставки
+        """
+        bet = 0
+        while bet not in range(1, self.bank+1):
+            print(f'Ваш банк: {self.bank}\nВведите вашу ставку:')
+            bet = int(input('>> '))
+            print()
         else:
-            break
-    
-    print('\n')
+            return bet
 
-    car_one.start()
-    car_two.start()
-
-    for i in range (11):
-
-        boost = randint(5, 15)
-        car_one.new_speed(boost=boost)
-        car_one.distance = i * car_one.speed
-
-        boost = randint(5, 15)
-        car_two.new_speed(boost=boost)
-        car_two.distance = i * car_two.speed
-
-        print(f'{i} секунда')
-
-        if car_one.distance > car_two.distance:
-            print( f'{car_one.name} лидирует со скоростью {car_one.speed}\nПройденное расстояние: {car_one.distance} метра')
-
-        elif car_one.distance < car_two.distance:
-            print( f'{car_two.name} лидирует со скоростью {car_two.speed}\nПройденное расстояние: {car_one.distance} метра')
-
+    # Рекурсивная функция run_second вызывает сама себя, с изменённым аргументом (second) пока все секунды не пройдут
+    def run_second(self, second=1):
+        time.sleep(0.5)
+        print("-" * 75, f'\n{second} секунда из {self.duration}')
+        for car in self.cars:
+            boost = randrange(5, 15)
+            car.speed += boost
+            car.distance = second * car.speed
+            print(f'Скорость {car.name} - {car.speed} км/ч, пройденное расстояние - {car.distance}')
+        leaders = [car.name for car in sorted(self.cars, key=attrgetter('distance'), reverse=True)][0]
+        if len(set([car.distance for car in self.cars])) == 1:
+            print('Удивительно, но все автомобили идут ноздря в ноздрю')
         else:
-            print(f'Удивительно, но оба автомобиля идут ноздря в ноздрю')
+            print(f'{leaders} лидирует со скоростью '
+                  f'{[car.__dict__["speed"] for car in self.cars if car.__dict__["name"] == leaders][0]} км/ч')
+        # print(f'Second = {second}, duration = {self.duration}') #
 
-        print('---------------------------------------------------------------------------')
-        time.sleep(3)
+        if second == self.duration:
+            # print(f'********{second, self.duration}********') #
+            # Если >1 участника имеют одинаковую скорость, равную максимальной из всех в текущую секунду:
+            if len(set([car.__dict__['name'] for car in self.cars
+                        if car.__dict__['speed'] == max([car.__dict__['speed'] for car in self.cars])])) > 1:
+                self.duration += 1
+                print('Добавочная секунда..')
+                self.run_second(second=self.duration)
+            else:
+                self.finish_race(leaders)
+        else:
+            self.run_second(second+1)
 
-    if car_one.distance>car_two.distance:
-        car_one.winner()
-        car_two.loser()
-    else:
-        car_two.winner()
-        car_one.loser()
+    def start_race(self):
+        for car in self.cars:
+            car.start(from_race=True)
+        print('■□' * 10)
+        self.run_second()
 
-    time.sleep(3)
+    def finish_race(self, winner: str):
+        print('■□'*10)
+        print(f'{winner} финиширует первым!')
+        winner_id = int([car.__dict__["id_"] for car in self.cars if car.__dict__["name"] == winner][0])
+        for car in self.cars:
+            # сбрасываем все изменяемые показатели автомобиля
+            car.finish(from_race=True)
+        if winner_id == self.selected_car:
+            cost = self.bet * 2
+            self.bank += cost
+            print(f'Ваша машина выиграла! Ваш выигрыш составил {cost}$'
+                  f'\nВаш банк в данный момент составляет {self.bank}$')
+        else:
+            self.bank -= self.bet
+            print(f'Вы проиграли(\nВаш банк в данный момент составляет {self.bank}$')
+        retry = input('Ещё? (Д/Н)\n>> ').lower()
+        match retry:
+            case 'д':
+                Race(duration=self.orig_duration, cars=self.cars, bank=self.bank)
+            case 'н':
+                print('Скрипнув дверью, вы ненадолго покидаете казино..')
 
-    if car_selected == car_one.id and car_one.win == 1:
-        cost = int(bet) * 0.25
-        bank = bank + cost
-        print(f'\nВаша машина выиграла!\nВаш выигрыш составил {cost}$\nВаш банк в данный момент составляет {bank}$')
-    else:
-        bank = bank - int(bet)
-        print(f'Вы проиграли(\nВаш банк в данный момент составляет {bank}$')
 
-    car_one.speed = 1
-    car_two.speed = 1
-    car_one.distance = 0
-    car_two.distance = 0
+cars_list = []  # Каждый создавшийся машин заносит себя в этот список
 
-while True:
-    race()
-    print('Ещё?')
-    txt = input()
-    if txt == 'Да':
-        race()
-    if txt == 'Нет':
-        print('Конец игры!')
-        break
+car_one = Vehicle(1, 'УАЗ')
+car_two = Vehicle(2, 'Camry')
+car_three = Vehicle(3, 'Мэтр')
+car_four = Vehicle(4, 'Каптюр')
+"""
+# Сразу несколько машин для теста:
 
-print('123')
+for i in range(len(cars_list)+1, len(cars_list)+10):
+    Vehicle(id_=i, name=f'Car{i}')
+"""
+
+# Создаём гонку:
+race = Race(duration=15, cars=cars_list, bank=1000)
